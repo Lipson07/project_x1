4b9946568b57ef881db23c517571e242e1483ab3
Марк Эмануэль <emanuel@OP8.local>
2025-02-10 16:15:13 +0300

    fix

--- backend/app/prestart.py
+++ backend/app/prestart.py
@@ -10,14 +10,7 @@ from alembic import command
 from app.core.config import ROOT
 
 
-<<<<<<< HEAD
-
-print(f'>>> ############# {str(ROOT.parent / "alembic.ini")}')
-#alembic_cfg = Config(ROOT.parent / "alembic.ini")
-alembic_cfg = Config()
-=======
 alembic_cfg = Config( str(ROOT.parent / "alembic.ini") )
->>>>>>> aa8afdca26826489b859cb681ad760b7d3d0d16d
 alembic_cfg.set_main_option('script_location', str(ROOT.parent / "alembic"))
 alembic_cfg.set_main_option('sqlalchemy.url', os.environ.get("SQLALCHEMY_DATABASE_URI"))
 
