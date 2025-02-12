e23564bfb991c86043ff3d49617e1900305438b0
Nikita <m.nekit2507@gmail.com>
2025-02-12 22:47:40 +0300

    New Personal_account

---
+++ backend/app/app/static/js/personal_account/profile.js
@@ -0,0 +1,19 @@
+document.getElementById('profile-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+  
+  document.getElementById('save-profile').addEventListener('click', () => {
+    const form = document.getElementById('profile-form');
+    if (form.checkValidity()) {
+      alert('Изменения успешно сохранены');
+    } else {
+      form.reportValidity();
+    }
+  });
+  
+  const inputs = document.querySelectorAll('.form-control:not([readonly])');
+  inputs.forEach(input => {
+    input.addEventListener('input', () => {
+      input.style.borderColor = input.value.trim() ? '#eee' : var(--danger);
+    });
+  });
