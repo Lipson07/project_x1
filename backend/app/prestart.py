0129607d6461be14e1199ec5390f690424607aa5
Данил Шифман <danilshifman07@gmail.com>
2025-02-12 10:45:23 +0300

    mmmm

---
+++ backend/app/prestart.py
@@ -0,0 +1,20 @@
+#/**
+# Create data base migration
+# **/
+import subprocess
+import sys, os
+from pathlib import Path
+
+from alembic.config import Config
+from alembic import command
+from app.core.config import ROOT
+
+
+alembic_cfg = Config( str(ROOT.parent / "alembic.ini") )
+alembic_cfg.set_main_option('script_location', str(ROOT.parent / "alembic"))
+alembic_cfg.set_main_option('sqlalchemy.url', os.environ.get("SQLALCHEMY_DATABASE_URI"))
+
+subprocess.run([sys.executable, "./app/backend_pre_start.py"])
+command.revision(alembic_cfg, "--autogenerate", autogenerate=True)  # Generate new migration file
+command.upgrade(alembic_cfg, "head")
+subprocess.run([sys.executable, "./app/initial_data.py"])
